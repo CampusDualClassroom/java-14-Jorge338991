311550e565f117c5cb1e357a5af1f2347c8840d1
Jorge338991 <jorge338991@gmail.com>
2024-11-23 12:01:00 +0100

    Se añade solución a Exercise14.java

--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -8,4 +8,8 @@ public class Person {
 		this.name = name;
 		this.surname = surname;
 	}
+
+	public void getDetails(){
+		System.out.println("El nombre de esta personas es: " + this.name + " " + this.surname);
+	}
 }
